a4e349a6487320cb3b5f23dd4c43b7025b6ef899
blakelower <blakelower1999@gmail.com>
2019-12-16 23:41:40 +0700

    had many issues with my code, was struggling a lot and had many bugs, found this to be hard but was able to do Display but not Controls

--- src/display/Display.spec.js
+++ src/display/Display.spec.js
@@ -4,53 +4,40 @@ import "@testing-library/jest-dom/extend-expect";
 import Display from "./Display";
 
 afterEach(rtl.cleanup);
-
-describe("Display", () => {
-  it("should match snapshot", () => {
-    const wrapper = rtl.render(<Display />);
-    expect(wrapper.baseElement).toMatchSnapshot();
-  });
-  it("renders without crashing", () => {
-    rtl.render(<Display />);
-  });
-  it("open and unlocked", () => {
-    const { getByText } = rtl.render(<Display closed={false} locked={false} />);
-    getByText(/Unlocked/);
-    getByText(/Open/);
-  });
-  it("closed and locked", () => {
-    const { getByText } = rtl.render(<Display closed={true} locked={true} />);
-    getByText(/Locked/);
-    getByText(/Closed/);
-  });
-  it("displays closed if the closed prop is true and open if otherwise", () => {
-    const { getByText } = rtl.render(<Display locked={false} closed={true} />);
-    getByText(/Closed/);
-  });
-  it("displays open if the closed prop is false and closed otherwise", () => {
-    const { getByText } = rtl.render(<Display locked={false} closed={false} />);
-    getByText(/Open/);
-  });
-  it("displays Locked if the locked prop is true and Unlocked if otherwise", () => {
-    let { getByText } = rtl.render(<Display locked={true} closed={true} />);
-    getByText(/Locked/);
-  });
-  it("displays Locked if the locked prop is true and Unlocked if otherwise", () => {
-    let { getByText } = rtl.render(<Display locked={false} closed={true} />);
-    getByText(/Unlocked/);
-  });
-  it("when locked or closed use the red-led class", () => {
-    const { getByText } = rtl.render(<Display locked={true} closed={true} />);
-    const closed = getByText(/Closed/);
-    const locked = getByText(/Locked/);
-    expect(closed.classList.contains("red-led")).toBeTruthy();
-    expect(locked.classList.contains("red-led")).toBeTruthy();
-  });
-  it("when unlocked or open use the green-led class", () => {
-    const { getByText } = rtl.render(<Display locked={false} closed={false} />);
-    const unlocked = getByText(/Unlocked/);
-    const open = getByText(/Open/);
-    expect(unlocked.classList.contains("green-led")).toBeTruthy();
-    expect(open.classList.contains("green-led")).toBeTruthy();
-  });
+let wrapper;
+let Unlocked = () => {
+  return wrapper.queryByText("Unlocked");
+};
+let Open = () => {
+  return wrapper.queryByText("Open");
+};
+let Closed = () => {
+  return wrapper.queryByText("Closed");
+};
+let Locked = () => {
+  return wrapper.queryByText("Locked");
+};
+it("not crashing", () => {
+  wrapper = rtl.render(<Display />);
+  expect(wrapper.container).toMatchSnapshot();
+});
+it("default props", () => {
+  wrapper = rtl.render(<Display closed={false} locked={false} />);
+  expect(wrapper.container).toMatchSnapshot();
+  expect(Unlocked()).toHaveClass("led", "green-led");
+  expect(Open()).toBeInTheDocument();
+  expect(Open()).toHaveClass("led", "green-led");
+});
+it("set to true", () => {
+  wrapper = rtl.render(<Display closed={true} locked={false} />);
+  expect(wrapper.container).toMatchSnapshot();
+  expect(Unlocked()).toBeVisible();
+  expect(Unlocked()).toHaveClass("led", "green-led");
+  expect(Closed()).toHaveClass("led", "red-led");
+});
+it("closed and locked to true", () => {
+  wrapper = rtl.render(<Display closed={true} locked={true} />);
+  expect(Locked()).toHaveClass("led", "red-led");
+  expect(Closed()).toBeInTheDocument();
+  expect(Closed()).toHaveClass("led", "red-led");
 });
